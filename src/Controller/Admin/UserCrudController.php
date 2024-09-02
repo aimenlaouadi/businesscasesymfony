799c1fefc4cf07fea6724b4fe01e2c14e70ed83d
aimen laouadi <alaouadi@stagiaire-humanbooster.com>
2024-09-02 17:23:01 +0200

    update user

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -4,9 +4,11 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\PasswordField;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -15,14 +17,31 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('lastname', 'Nom de famille'),
+            TextField::new('firstname', 'Prénom'),
+            TextField::new('telephone', 'Téléphone'),
+            TextField::new('username', 'Nom d\'utilisateur'),
+            TextField::new('password', 'Mot de passe')
+            ->setFormType(PasswordType::class)
+            ->onlyOnForms(),
+
+
+            // Champ pour gérer les rôles de l'utilisateur
+            ChoiceField::new('roles', 'Rôles')
+                ->setChoices([
+                    'Administrateur' => 'ROLE_ADMIN',
+                    'Utilisateur' => 'ROLE_USER',
+                ])
+                ->allowMultipleChoices()
+
+
+
+
+
         ];
     }
-    */
 }
